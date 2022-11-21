34644726c23e2dc0d588d25b217d2591bfd3f8ac
Salvatore Vella <salvatorevella1234@gmail.com>
2022-11-21 14:24:35 +0100

    Exercise correction

--- exercise.js
+++ exercise.js
@@ -4,9 +4,8 @@ const user = {
   age: 25,
 };
 const localUserCookie = (user) => {
-  const getName = JSON.stringify(Object.keys({user})[0]);
   const userName = JSON.stringify(user);
-  return localStorage.setItem(getName, userName);
+  return localStorage.setItem("user", userName);
 }
 
 localUserCookie(user);
